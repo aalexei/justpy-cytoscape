5a189eb55cc7e4575d0173ec947badbda150e9ac
Alexei <alexei@entropy.energy>
2022-06-21 22:54:29 +1000

    Adding some convenience methods

--- justpy_cytoscape/cytoscapecomponent.py
+++ justpy_cytoscape/cytoscapecomponent.py
@@ -28,6 +28,30 @@ class Cytoscape(jp.JustpyBaseComponent):
     def react(self, data):
         pass
 
+    async def reset(self, websocket):
+        '''Reset the graph to the default zoom level and panning position.'''
+        return await self.run_method('reset()', websocket)
+
+    async def fit(self, websocket):
+        '''Pan and zooms the graph to fit to a collection.'''
+        return await self.run_method('fit()', websocket)
+
+    async def center(self, websocket):
+        '''Centre on all elements in the graph.'''
+        return await self.run_method('center()', websocket)
+
+    async def pan(self, x, y, websocket):
+        '''Set the current panning position to (x,y)'''
+        return await self.run_method(f'pan({{x:{x}, y:{y}}})', websocket)
+
+    async def panBy(self, x, y, websocket):
+        '''Shift rendered position by vector (x,y)'''
+        return await self.run_method(f'panBy({{x:{x}, y:{y}}})', websocket)
+
+    async def add(self, data, websocket):
+        '''Add elements to the graph'''
+        return await self.run_method(f'add({data})', websocket)
+
     def convert_object_to_dict(self):
         d = {}
         d['vue_type'] = self.vue_type
